b51e162d4efc039028191b2ba50c8404fb0bdf54
Saurabh Kushwaha <saurabhkushwaha9889@gmail.com>
2026-06-08 15:39:56 +0530

    Fix: auth complete

--- src/bootstrap/authBootstrap.ts
+++ src/bootstrap/authBootstrap.ts
@@ -1,10 +1,13 @@
 import { store } from "@/store/store";
 import { authService } from "@/bootstrap/bootstrap.api";
-import { unauthenticated, setUser, authenticated } from "@/store/slices/auth.slice";
+import { unauthenticated, setUser, authenticated, authChecking } from "@/store/slices/auth.slice";
 
 export const authBootstrap = async () => {
     try {
       store.dispatch(unauthenticated());
+      store.dispatch(setUser(null));
+      store.dispatch(authChecking());
+
       const user = await authService.getMe();
       store.dispatch(authenticated());
       store.dispatch(setUser(user));
